9281cf091c6438e190eccdcee1b1744eda17f3e3
Rami Grossman <ramigg@gmail.com>
2011-03-08 13:32:19 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kmedia::Application.config.secret_token = '44120ca453f36d7cc64c514bb78abc3a4739d76400b67ea0be006e1d425d6c758f8f12a17ef454f659e460a564403645392a55a5020a4ac14448f47abf1cfb62'
