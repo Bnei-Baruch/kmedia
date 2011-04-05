3187ed00ff06f259b1561aeab49712b09c6ed90e
gshilin <gshilin@gmail.com>
2011-04-05 12:33:12 +0300

    1. Automatic fields update 2. 3 first descriptions in correct order

--- lib/string_parser.rb
+++ lib/string_parser.rb
@@ -7,7 +7,7 @@ class StringParser
     year = @string[/((19|20)\d\d)-(0?[1-9]|1[012])-([^a-z0-9][1-9]|0[1-9]|[12][0-9]|3[01])/, 1]
     month = @string[/((19|20)\d\d)-(0?[1-9]|1[012])-([^a-z0-9][1-9]|0[1-9]|[12][0-9]|3[01])/, 3]
     day = @string[/((19|20)\d\d)-(0?[1-9]|1[012])-([^a-z0-9][1-9]|0[1-9]|[12][0-9]|3[01])/, 4]
-    [year, month, day]
+    [year.to_i, month.to_i, day.to_i]
   end
 
   def language
@@ -15,4 +15,7 @@ class StringParser
     @string[/([^a-z0-9])(#{lang})([^a-z0-9])/,2]
   end
 
+  def lecturer_rav?
+    @string =~ /^(rav_)|(_rav_)/
+  end
 end
