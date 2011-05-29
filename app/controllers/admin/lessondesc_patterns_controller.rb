c61199b25b3cd0299a3fc35c87ccb41bcf63d5f7
gshilin <gshilin@gmail.com>
2011-05-29 12:48:37 +0300

    paginate container description patterns list

--- app/controllers/admin/lessondesc_patterns_controller.rb
+++ app/controllers/admin/lessondesc_patterns_controller.rb
@@ -3,7 +3,7 @@ class Admin::LessondescPatternsController < ApplicationController
   before_filter :common_set, :only => [:new, :create, :edit, :update]
 
   def index
-    @lessondesc_patterns = LessondescPattern.all
+    @lessondesc_patterns = LessondescPattern.page(params[:page]).per(50)
   end
 
   def show
