8ca68f412e03290eaf44fc5cb3e4d27fa624b83b
gshilin <gshilin@gmail.com>
2011-06-16 10:21:42 +0300

    destroy link should destroy and not edit :)

--- app/controllers/admin/lessons_controller.rb
+++ app/controllers/admin/lessons_controller.rb
@@ -28,7 +28,7 @@ class Admin::LessonsController < ApplicationController
     @lesson = Lesson.new(params[:lesson])
     authorize! :create, @lesson
     if @lesson.save
-      redirect_to admin_lesson_path(@lesson), :notice => "Successfully created admin/lesson."
+      redirect_to admin_lesson_path(@lesson), :notice => "Successfully created admin/container."
     else
       params[:lesson][:lesson_descriptions_attributes].each do |k, v|
         @lesson.lesson_descriptions.build(v) if v[:lessondesc].blank?
@@ -53,7 +53,7 @@ class Admin::LessonsController < ApplicationController
     @lesson = Lesson.find(params[:id])
     authorize! :update, @lesson
     if @lesson.update_attributes(params[:lesson])
-      redirect_to admin_lesson_path(@lesson), :notice  => "Successfully updated admin/lesson."
+      redirect_to admin_lesson_path(@lesson), :notice  => "Successfully updated admin/container."
     else
       params[:lesson][:lesson_descriptions_attributes].each do |k, v|
         @lesson.lesson_descriptions.build(v) if v[:lessondesc].blank?
@@ -67,7 +67,7 @@ class Admin::LessonsController < ApplicationController
     @lesson = Lesson.find(params[:id])
     authorize! :destroy, @lesson
     @lesson.destroy
-    redirect_to admin_lessons_url, :notice => "Successfully destroyed admin/lesson."
+    redirect_to admin_lessons_url, :notice => "Successfully destroyed admin/container."
   end
 
   def parse_lesson_name
