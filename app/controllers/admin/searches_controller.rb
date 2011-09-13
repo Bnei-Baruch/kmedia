14e246373119f5faf0df76dad899b7bad95b063d
gshilin <gshilin@gmail.com>
2011-09-13 12:21:16 +0300

    changes in reindexing

--- app/controllers/admin/searches_controller.rb
+++ app/controllers/admin/searches_controller.rb
@@ -14,7 +14,7 @@ class Admin::SearchesController < ApplicationController
   private
   def set_fields
     if params[:search]
-      query = params[:search][:search]
+      query = params[:search][:search].gsub(/-/, '_')
     else
       query = ''
     end
