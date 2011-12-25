c5193cef885dbfd6125520ef896de4cb43551180
Gregory Shilin <gshilin@gmail.com>
2011-12-25 12:44:41 +0000

    Bug: Missing host to link to!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :authenticate_user! #, :except => [:some_action_without_auth]
+  before_filter :mailer_set_url_options
+ 
+  def mailer_set_url_options
+    ActionMailer::Base.default_url_options[:host] = request.host_with_port
+  end
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => exception.message
