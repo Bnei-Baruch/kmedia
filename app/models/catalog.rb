c509f50029dea84d2f691e124d52e498bf9bd2dd
amikhile <annamik@gmail.com>
2012-10-13 14:24:22 +0200

    fixing pull request comments

--- app/models/catalog.rb
+++ app/models/catalog.rb
@@ -24,7 +24,7 @@ class Catalog < ActiveRecord::Base
 
   class ParentValidator < ActiveModel::Validator
     def validate(catalog)
-      catalog.errors[:parentnodeid]<< "Catalog can't be a parent of himself" if catalog.parentnodeid = catalog.catalognodeid
+      catalog.errors[:parentnodeid] << "Catalog can't be a parent of himself" if catalog.parentnodeid == catalog.catalognodeid
     end
   end
 
