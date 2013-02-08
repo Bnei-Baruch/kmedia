970f5f671c758ef0ca90c81709b6f9f15ddc16d0
amikhile <annamik@gmail.com>
2013-02-08 15:26:37 +0100

    changing style

--- app/models/catalog.rb
+++ app/models/catalog.rb
@@ -47,7 +47,7 @@ class Catalog < ActiveRecord::Base
     if id.blank?
       children_catalogs = Catalog.secure(secure).roots
     else
-      catalog = Catalog.secure(secure).where(:catalognodeid => id).first
+      catalog = Catalog.secure(secure).where(catalognodeid: id).first
       children_catalogs = catalog.children
     end
   end
