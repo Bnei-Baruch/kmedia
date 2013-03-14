6dd1842b3e437d9bb87ea91ee970ec1af12c6b4d
Gregory Shilin <gshilin@gmail.com>
2013-03-14 14:43:27 +0000

    Setup devise to sent emails from archive@kabalahmedia.info

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,6 +44,7 @@ Kmedia::Application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :sendmail
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable threaded mode
   # config.threadsafe!
