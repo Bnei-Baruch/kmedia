c1f11faca92ce6ed9b2727577c8b7134917ccf2c
Gregory Shilin <gshilin@gmail.com>
2013-03-15 06:01:58 +0200

    Last second changes

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -1,37 +1,15 @@
-class Admin::SelectedCatalogsController < Admin::ApplicationController
-  authorize_resource class: Catalog
+class Admin::CommentsController < Admin::ApplicationController
+  load_resource :only => [:destroy]
+  authorize_resource class: Comment
 
   def index
-    @catalogs = Catalog.selected_catalogs_ar('ENG').includes(:parent)
+    @comments = Comment.order('id DESC').page(params[:page])
   end
 
-  def new
-    @catalog = Catalog.new
+  def destroy
+    @comment.destroy
+
+    redirect_to admin_comments_url, :notice => "Successfully destroyed comment."
   end
 
-  def set_selected
-    name = params[:catalog][:catalognodename]
-    catalog = Catalog.where(catalognodename: name).first
-    if catalog && catalog.update_attribute(:selected_catalog, true)
-      redirect_to admin_selected_catalogs_url, :notice => "Successfully selected catalog."
-    else
-      @catalog = Catalog.new({catalognodename: name})
-      @catalog.errors.add(:catalognodename, "Catalog \"#{name}\" does not exist.")
-      render :new
-    end
-  end
-
-  def unset_selected
-    @catalog = Catalog.find(params[:id])
-    if @catalog.update_attribute(:selected_catalog, :false)
-      redirect_to :action => 'index', :notice => "Successfully unselected catalog."
-    else
-      render :index, notice: "Unable to unselect catalog #{@catalog.catalognodename}"
-    end
-  end
-
-  def autocomplete
-    catalogs = Catalog.order(:catalognodename).where('catalognodename like ?', "#{params[:term]}%").pluck(:catalognodename)
-    render json: catalogs
-  end
 end
