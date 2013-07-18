372700f72204ff2c47698cdeda17a978a67f57f7
Gregory Shilin <gshilin@gmail.com>
2013-07-18 12:55:23 +0300

    Если выбрать все каталоги, то в списке появятся каталоги с названиями на английском языке, и продублируются на выборном языке

--- app/models/catalog.rb
+++ app/models/catalog.rb
@@ -107,7 +107,11 @@ class Catalog < ActiveRecord::Base
   end
 
   def self.all_catalogs_with_descriptions(language_code3, secure = 0)
-    catalogs = Catalog.secure(secure).joins(:catalog_descriptions).where('catnodedesc.lang = ? OR catnodedesc.lang = "ENG"', language_code3).order('catalognodename ASC')
+    catalogs = Catalog.secure(secure).joins(:catalog_descriptions)
+    .where(
+        '`catnodedesc`.lang = ? OR (`catnodedesc`.lang = "ENG" AND NOT EXISTS(SELECT * FROM `catnodedesc` C WHERE `catalognode`.catalognodeid = `C`.catalognodeid AND `C`.lang = ?))',
+        language_code3, language_code3)
+    .order('catalognodename ASC')
     catalogs.multipluck(:'catalognode.catalognodeid as catalognodeid', :'COALESCE(catnodedesc.catalognodename, catalognode.catalognodename) as catalognodename', :'catalognode.parentnodeid as parentnodeid')
   end
 
