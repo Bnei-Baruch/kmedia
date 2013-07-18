21e2cf021905200dad8113401e74818c23984974
Gregory Shilin <gshilin@gmail.com>
2013-07-18 10:04:18 +0300

    hide secret token in .env file

---
+++ config/initializers/dotenv.rb
@@ -0,0 +1,51 @@
+module Dotenv
+  class FormatError < SyntaxError
+  end
+
+  class Environment < Hash
+    LINE = /
+      \A
+      (?:export\s+)?    # optional export
+      ([\w\.]+)         # key
+      (?:\s*=\s*|:\s+?) # separator
+      (                 # value begin
+        '(?:\'|[^'])*'  #   single quoted value
+        |               #   or
+        "(?:\"|[^"])*"  #   double quoted value
+        |               #   or
+        [^#\n]+         #   unquoted value
+      )                 # value end
+      (?:\s*\#.*)?      # optional comment
+      \z
+    /x
+
+    def initialize(filename)
+      @filename = filename
+      load
+    end
+
+    def load
+      read.each do |line|
+        if match = line.match(LINE)
+          key, value = match.captures
+          value = value.strip.sub(/\A(['"])(.*)\1\z/, '\2')
+          value = value.gsub('\n', "\n").gsub(/\\(.)/, '\1') if $1 == '"'
+          self[key] = value
+        elsif line !~ /\A\s*(?:#.*)?\z/ # not comment or blank line
+          raise FormatError, "Line #{line.inspect} doesn't match format"
+        end
+      end
+    end
+
+    def read
+      File.read(@filename).split("\n")
+    end
+
+    def apply
+      each { |k, v| ENV[k] ||= v }
+    end
+  end
+
+  filename = File.expand_path('.env')
+  Environment.new(filename).apply if File.exists?(filename)
+end
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Kmedia::Application.config.secret_token = '44120ca453f36d7cc64c514bb78abc3a4739d76400b67ea0be006e1d425d6c758f8f12a17ef454f659e460a564403645392a55a5020a4ac14448f47abf1cfb62'
+Kmedia::Application.config.secret_token = ENV['SECRET_TOKEN']
