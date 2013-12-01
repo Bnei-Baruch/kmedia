dea6c8e52485189d8ec54f1cd339296cfa17639c
Gregory Shilin <gshilin@gmail.com>
2013-12-01 17:22:38 +0200

    api changes

--- config/routes.rb
+++ config/routes.rb
@@ -119,6 +119,7 @@ Kmedia::Application.routes.draw do
         collection do
           post :register_file, :get_file_servers
           post :content_types, :file_types, :catalogs, :languages, :file_ids, :files, :patterns
+          get :patterns
         end
         member do
         end
