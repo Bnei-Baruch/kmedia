4a3a2790bc9ba9b33d00a66979dddaf50825a3ef
Gregory Shilin <gshilin@gmail.com>
2014-05-05 05:29:25 +0300

    cosmetic stuff

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -10,7 +10,7 @@ class Admin::ApplicationController < ApplicationController
   end
 
   rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_url, :alert => exception.message
+    redirect_to root_url, alert: exception.message
   end
 
 end
