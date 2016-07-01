5a8d9bc56d292a80cc55fab4d296edfc70d9375e
Gregory Shilin <gshilin@gmail.com>
2016-07-01 09:13:31 +0300

    return morning lessons to lesson downloader

--- app/controllers/morning_lesson_controller.rb
+++ app/controllers/morning_lesson_controller.rb
@@ -1,18 +1,17 @@
 class MorningLessonController < ApplicationController
   def show
-    @lang = params[:id]
+    @lang         = params[:id]
 
     # find the morning lessons
-    search       = Search.new(content_type_id: Container::LESSON_CONTENT_TYPE_ID, date_from: Date.yesterday.to_s)
+    search        = Search.new(content_type_id: Container::LESSON_CONTENT_TYPE_ID, date_from: Date.yesterday.to_s)
     search_result = search.search_full_text(1)
     #handle the error
     return render json: { error: search.error } unless search_result
 
     lessons   = search_result.results
-    files      = lessons.map(&:file_assets).flatten
-    @language = {}
-    lang_hash = files.group_by(&:lang).each do |lang, files_by_lang|
-      @language = files_by_lang.group_by { |f| f.date.to_date } if lang == @lang
-    end
+    files     = lessons.map(&:file_assets).flatten
+    @language = files.select { |f| f.lang == @lang }.group_by { |f| f.date.to_date }
+
+    render layout: false
   end
 end
