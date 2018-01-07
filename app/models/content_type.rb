325c2a7d1b78c4da8cd9c222b15e1648aa9140ab
Gregory Shilin <gshilin@gmail.com>
2018-01-07 15:42:53 +0200

    add new content type: children-lesson

--- app/models/content_type.rb
+++ app/models/content_type.rb
@@ -12,14 +12,14 @@ class ContentType < ActiveRecord::Base
   has_many :lessons
 
   CONTENT_TYPE_ID = {}
-  CONTENT_TYPE = {}
+  CONTENT_TYPE    = {}
 
-  %w(all lesson lecture program song clip book declamation ekronot vl seuda art publication).each do |name|
-    ct = ContentType.where(pattern: name).first
+  %w(all lesson lecture program song clip book declamation ekronot vl seuda art publication children-lesson).each do |name|
+    ct                    = ContentType.where(pattern: name).first
     CONTENT_TYPE_ID[name] = ct.try(:id)
-    CONTENT_TYPE[name] = ct
+    CONTENT_TYPE[name]    = ct
   end
 
   CONTENT_PROGRAM = CONTENT_TYPE['program']
-  CONTENT_BOOK = CONTENT_TYPE['book']
+  CONTENT_BOOK    = CONTENT_TYPE['book']
 end
